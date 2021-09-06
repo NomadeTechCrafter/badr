d1c66acde38a7f08e3a9c100a52c9df9648bf8c4
Marouane RAIS <marouane.rais@gmail.com>
2021-09-06 18:28:35 +0100

    alignement dedouanement web

--- src/commons/component/modules/rechercheRefDum/ComRedressementRechercheRefComp.js
+++ src/commons/component/modules/rechercheRefDum/ComRedressementRechercheRefComp.js
@@ -27,11 +27,11 @@ import * as ConsulterDumAction from '../../../state/actions/ConsulterDumAction';
 
 class ComRedressementRechercheRefComp extends Component {
   defaultState = {
-    bureau: '309',
-    regime: '085',
-    annee: '2021',
-    serie: '0000011',
-    cle: 'S',
+    bureau: '',
+    regime: '',
+    annee: '',
+    serie: '',
+    cle: '',
     cleValide: '',
     login: '',
     numeroVoyage: '',
@@ -127,6 +127,7 @@ class ComRedressementRechercheRefComp extends Component {
     });
     this.props.dispatch(action);
     this.switchToChooseCommand();
+    console.log('========================================> ' + this.props?.fromWhere)
   };
 
   confirm = () => {
