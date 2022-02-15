95fc6ccb6b1c6a0bfe1c409323e0e818cc4482b4
Marouane RAIS <marouane.rais@gmail.com>
2022-02-15 16:36:41 +0100

    evolutions ECOREXPORT

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//appdynamics
+import { Instrumentation } from '@appdynamics/react-native-agent';
+ 
+ Instrumentation.start({
+     appKey: "EUM-AAB-AUN",
+     collectorURL: "http://badr.douane.gov.ma/appdEum/"
+});
